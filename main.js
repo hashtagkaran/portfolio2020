adb3f573a19220b49b6d45fc1b31481a0f4a9284
Karan SINGH <thekrsingh@gmail.com>
2020-11-07 08:15:42 +0530

    portfolio 2020

---
+++ main.js
@@ -0,0 +1,14 @@
+function aboutHome() {
+	document.querySelector("#aboutHome").scrollIntoView({
+		behavior: "smooth",
+	});
+}
+function goToTop() {
+	document.querySelector("#introHome").scrollIntoView({
+		behavior: "smooth",
+	});
+}
+
+function blogPage() {
+window.location.replace("https://hashtagkaran.wordpress.com/");
+}
